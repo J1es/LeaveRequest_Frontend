37b35166499acbaea06b6b31e2d226faa9c612ce
J1es <135467990+J1es@users.noreply.github.com>
2026-09-13 21:58:53 +0100

    -Fixed problem with testing

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -3,48 +3,66 @@
 const loginURL: string = "http://localhost:5173/login";
 
 Cypress.Commands.add("loginStaff", () => {
-    cy.visit(loginURL);
 
-    cy.get('input[name="email"]')
-        .type("staff-test@email.com");
+    cy.session("staff", () => {
 
-    cy.get('input[name="password"]')
-        .type("staff-test");
+        cy.visit(loginURL);
 
-    cy.contains("button", "Sign In")
-        .click();
+        cy.get('input[name="email"]')
+            .type("staff-test@email.com");
 
-    cy.url().should("eq", "http://localhost:5173/");
+        cy.get('input[name="password"]')
+            .type("staff-test");
+
+        cy.contains("button", "Sign In")
+            .click();
+
+        cy.url()
+            .should("eq", "http://localhost:5173/");
+    });
+
 });
 
 Cypress.Commands.add("loginManager", () => {
-    cy.visit(loginURL);
 
-    cy.get('input[name="email"]')
-        .type("manager-test@email.com");
+    cy.session("manager", () => {
 
-    cy.get('input[name="password"]')
-        .type("manager-test");
+        cy.visit(loginURL);
 
-    cy.contains("button", "Sign In")
-        .click();
+        cy.get('input[name="email"]')
+            .type("manager-test@email.com");
 
-    cy.url().should("eq", "http://localhost:5173/");
+        cy.get('input[name="password"]')
+            .type("manager-test");
+
+        cy.contains("button", "Sign In")
+            .click();
+
+        cy.url()
+            .should("eq", "http://localhost:5173/");
+    });
+
 });
 
 Cypress.Commands.add("loginAdmin", () => {
-    cy.visit(loginURL);
 
-    cy.get('input[name="email"]')
-        .type("admin-test@email.com");
+    cy.session("admin", () => {
 
-    cy.get('input[name="password"]')
-        .type("admin-test");
+        cy.visit(loginURL);
 
-    cy.contains("button", "Sign In")
-        .click();
+        cy.get('input[name="email"]')
+            .type("admin-test@email.com");
 
-    cy.url().should("eq", "http://localhost:5173/");
+        cy.get('input[name="password"]')
+            .type("admin-test");
+
+        cy.contains("button", "Sign In")
+            .click();
+
+        cy.url()
+            .should("eq", "http://localhost:5173/");
+    });
+
 });
 
 //eslint-disable-next-line
